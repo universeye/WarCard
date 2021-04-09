91e5016f453714f49118e2193074faff170dcb2b
Terry <terrykuo501@gmail.com>
2021-04-09 13:21:06 +0800

    War Card Game Initial Commit

--- WarCard/ContentView.swift
+++ WarCard/ContentView.swift
@@ -9,8 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.green.ignoresSafeArea()
+            VStack {
+                Image(uiImage: #imageLiteral(resourceName: "tfc_logo"))
+                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Spacer()
+                HStack(spacing: 50) {
+                    Image(uiImage: #imageLiteral(resourceName: "3C"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 150, alignment: .center)
+                    
+                    Image(uiImage: #imageLiteral(resourceName: "3C"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 150, alignment: .center)
+                }
+                Spacer()
+                Button(action: {}, label: {
+                    Image(uiImage: #imageLiteral(resourceName: "buttons167"))
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 280, height: 70, alignment: .center)
+                        .overlay(Text("Play")
+                                    .foregroundColor(.black))
+                })
+                Spacer()
+                HStack {
+                    VStack {
+                        Text("Player")
+                            .padding()
+                        Text("0")
+                    }
+                    
+                    VStack {
+                        Text("Player")
+                            .padding()
+                        Text("0")
+                    }
+                }
+                Spacer()
+            }
+        }
     }
 }
 
